c735ac25dca063a7c39a26345cb711d6e67e2f4b
Richard <rkerr022@gmail.com>
2026-04-20 21:26:22 -0400

    added face detect feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,98 @@ import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
 import ImageLinkForm from './components/image_link_form/ImageLinkForm'
 import Rank from './components/rank/Rank'
+import FaceRecognition from './components/face_recognition/FaceRecognition'
 import ParticlesBg from 'particles-bg'
+import { useState } from 'react'
 import './App.css'
 
-const App = () => {
+const App  = () => {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('')
+  const [box, setBox] = useState({})
+
+  const onInputChange = (event) => {
+    setInput(event.target.value)
+  
+  }
+
+  const calculateFaceLocation = (regions) => {
+    const faceRegion = regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height = Number(image.height)
+    return {
+      leftCol: faceRegion.left_col * width,
+      topRow: faceRegion.top_row * height,
+      rightCol: width - (faceRegion.right_col * width),
+      bottomRow: height - (faceRegion.bottom_row * height)
+    }
+  }
+
+  const displayFaceBox = (box) => {
+    console.log(box)
+    setBox(box)
+  }
+
+
+  const onButtonSubmit = (requestOptions) => {
+    setImageUrl(input)
+    const MODEL_ID = 'face-detection';
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            const regions = result.outputs[0].data.regions;
+            displayFaceBox(calculateFaceLocation(regions))
+        })
+        .catch(error => console.log('error', error));
+  }
+
+  const setUpClarifai = () => {
+
+    const PAT = import.meta.env.VITE_PAT;
+    const USER_ID = import.meta.env.VITE_USER_ID;
+    const APP_ID = import.meta.env.VITE_APP_ID;
+    const MODEL_ID = 'face-no-depiction-detection';
+    const IMAGE_URL = input;
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                        // "base64": IMAGE_BYTES_STRING
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+    
+    return requestOptions
+  }
+  const onSubmit = () => {
+    onButtonSubmit(setUpClarifai())
+  }
   return (
     <>
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
+      <ImageLinkForm value={input} onInputChange={onInputChange} onSubmit={onSubmit} />
+      <FaceRecognition box={box} imageUrl={imageUrl}/>
       <ParticlesBg type="circle" bg={true} />
     </>
   )
--- src/components/image_link_form/ImageLinkForm.jsx
+++ src/components/image_link_form/ImageLinkForm.jsx
@@ -1,5 +1,5 @@
 import './ImageLinkForm.css'
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onSubmit}) => {
     return(
         <div>
             <p className="text-2xl">
@@ -7,8 +7,8 @@ const ImageLinkForm = () => {
             </p>
             <div className="center">
                 <div className='form center p-8 rounded-md shadow-2xl'>
-                    <input className=" text-xl p-2 w-[70%] mx-auto bg-white" type="text" />
-                    <button className="w-[30%] text-xl transition-transform hover:scale-105 inline-block no-underline px-4 py-2 bg-purple-400 text-white ">Detect</button>
+                    <input className=" text-xl p-2 w-[70%] mx-auto bg-white" type="text" onChange={onInputChange}/>
+                    <button className="w-[30%] text-xl transition-transform hover:scale-105 inline-block no-underline px-4 py-2 bg-purple-400 text-white " onClick={onSubmit}>Detect</button>
                 </div>
             </div>
         </div>
